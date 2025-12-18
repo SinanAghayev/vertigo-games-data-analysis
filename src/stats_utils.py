09b607efe5b9887cd87060f29c0e3c2214390cf4
Sinan-Aghayev <91735589+Sinan-Aghayev@users.noreply.github.com>
2025-12-18 16:20:04 +0300

    DAU calculation

--- src/stats_utils.py
+++ src/stats_utils.py
@@ -37,3 +37,32 @@ def fit_retention_curve(
     parameters, _ = curve_fit(retention_formula, days, retentions)
     a, b = parameters
     return a, b
+
+
+def calculate_daily_active_users(
+    day_number: int, installs_per_day: int, parameters: tuple[float, float]
+) -> int:
+    """
+        Calculates daily active users count for given day count, same day install are excluded.
+
+        Uses the formula
+            DAU(x) = N * sum(R(i))
+        where:
+            x is the day_number,
+            N is installs_per_day,
+            R(i) is retention on day i after install
+            i goes from 1 to x.
+
+    Args:
+        day_number (int): Day to find the DAU for.
+        installs_per_day (int): Assuming install count is the same everyday
+        parameters (tuple[float, float]): Parameters for retention formula (a, b)
+
+    Returns:
+        int: Total DAU after 'day_number' days
+    """
+    a, b = parameters
+    result = installs_per_day * sum(
+        [retention_formula(day, a, b) for day in range(1, day_number + 1)]
+    )
+    return int(result)
